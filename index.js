ab926f847d1a76b85f230e6120cd15bef07cd627
matsub <matsub.rk@gmail.com>
2018-12-02 02:10:16 +0900

    implement bookmark

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+const Datastore = require('@google-cloud/datastore')
+const datastore = Datastore()
+
+
+function register(url) {
+  if (url === '') {
+    return 'Usage: `/bookmark <something-to-bookmark>`'
+  }
+
+  const key = datastore.key('bookmark')
+  const entity = { key, data: { url } }
+
+  return datastore.save(entity)
+    .then(() => '覚えたよ')
+    .catch(err => {
+      console.error(err)
+      return '覚えらんねえ'
+    })
+}
+
+
+function pick() {
+  const query = datastore.createQuery('bookmark')
+
+  return datastore.runQuery(query)
+    .then(([[entity]]) => 'これ読め: ' + entity.url)
+    .catch(err => {
+      console.error(err)
+      return 'ちょっと今忙しいわ'
+    })
+}
+
+
 function makeMessage (text) {
   return {
     response_type: 'in_channel',
@@ -5,7 +38,20 @@ function makeMessage (text) {
   }
 }
 
-exports.slackBookmark = (req, res) => {
-  const response = makeMessage("Hello, Slack!")
-  res.send(JSON.stringify(response));
-};
+
+exports.slackBookmark = async (req, res) => {
+  const text = await register(req.body.text)
+  const msg = makeMessage(text)
+
+  res.setHeader('Content-Type', 'application/json')
+  res.send(JSON.stringify(msg))
+}
+
+
+exports.slackBookmarkPickRandomly = async (req, res) => {
+  const text = await pick()
+  const msg = makeMessage(text)
+
+  res.setHeader('Content-Type', 'application/json')
+  res.send(JSON.stringify(msg))
+}
